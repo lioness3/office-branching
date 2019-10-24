6e66f0102353bb48c1b415fd9289161954465136
Student <example@example.com>
2019-10-24 15:23:25 -0700

    fixed if/else statements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
 
+ var advice = $('#advice:checked').val();
+ var rules = $('#rules:checked').val();
+ var offended = $('#offended:checked').val();
+ var wellmeaning = $('#wellmeaning:checked').val();
+ var offensive = $('#offensive:checked').val();
+ var lazy = $('#lazy:checked').val();
+var unorganized = $('#unorganized:checked').val();
+
   if (advice) {
-    (".dwight").show();
+    $(".dwight").show();
   } else if (rules && offended) {
-    (".angela").show();
+    $(".angela").show();
   } else if (wellmeaning && rules) {
-    (".pam").show();
+    $(".pam").show();
   } else if (wellmeaning && offensive) {
-    (".michael").show();
+    $(".michael").show();
   } else if (lazy || unorganized) {
-    (".kevin").show();
+    $(".kevin").show();
   } else {
     alert("I told you to choose two!")
   };
+
 
 
 
