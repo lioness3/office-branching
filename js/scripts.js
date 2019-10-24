fd1ddc65b0cc100c4a10e5e7f7a5960f70f7019a
Student <example@example.com>
2019-10-24 14:59:51 -0700

    add else-if statements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
 
+  if (advice) {
+    (".dwight").show();
+  } else if (rules && offended) {
+    (".angela").show();
+  } else if (wellmeaning && rules) {
+    (".pam").show();
+  } else if (wellmeaning && offensive) {
+    (".michael").show();
+  } else if (lazy || unorganized) {
+    (".kevin").show();
+  } else {
+    alert("I told you to choose two!")
+  };
 
 
 
 
 
-  
 });
